be9fb7fb1ed19d74f2b4e88d656ca73075f5588a
Krzysztof Zmij <krzysztof.zmij@ingservicespolska.pl>
2018-08-02 09:47:36 +0200

    correct the mock UserServiceImpl to be thread-safe

--- src/main/scala/com/ing/wbaa/gargoyle/sts/service/UserService.scala
+++ src/main/scala/com/ing/wbaa/gargoyle/sts/service/UserService.scala
@@ -42,22 +42,22 @@ trait UserService {
  */
 class UserServiceImpl extends UserService {
 
-  private val storage = mutable.Map[String, UserInfo]()
+  private[this] val storage = mutable.Map[String, UserInfo]()
 
   //TODO test mock user
   storage.put(userKey("okAccessKey", "okSessionToken"), UserInfo("testUser", "secretKey", Seq("groupOne", "groupTwo"), "arn"))
 
   private def userKey(accessKey: String, sessionKey: String): String = s"$accessKey-$sessionKey"
 
-  override def isCredentialActive(accessKey: String, sessionToken: String): Boolean = {
+  override def isCredentialActive(accessKey: String, sessionToken: String): Boolean = synchronized {
     storage.get(userKey(accessKey, sessionToken)).isDefined
   }
 
-  override def getUserInfo(accessKey: String, sessionToken: String): Option[UserInfo] = {
+  override def getUserInfo(accessKey: String, sessionToken: String): Option[UserInfo] = synchronized {
     storage.get(userKey(accessKey, sessionToken))
   }
 
-  override def addUserInfo(accessKey: String, sessionToken: String, userInfo: UserInfo): Unit = {
+  override def addUserInfo(accessKey: String, sessionToken: String, userInfo: UserInfo): Unit = synchronized {
     storage.put(userKey(accessKey, sessionToken), userInfo)
   }
 }
