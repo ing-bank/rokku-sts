4710be3b1dd972806b80ac6f6b0d49a6d51cd93b
IT42KA <adam.rempter@ing.com>
2018-08-21 16:47:48 +0200

    update tests with assert

--- src/test/scala/com/ing/wbaa/gargoyle/sts/api/STSApiTest.scala
+++ src/test/scala/com/ing/wbaa/gargoyle/sts/api/STSApiTest.scala
@@ -9,13 +9,13 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.ing.wbaa.gargoyle.sts.data
 import com.ing.wbaa.gargoyle.sts.data._
 import com.ing.wbaa.gargoyle.sts.data.aws._
-import org.scalatest.{ Matchers, WordSpec }
+import org.scalatest.{ DiagrammedAssertions, WordSpec }
 
 import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 import scala.xml.NodeSeq
 
-class STSApiTest extends WordSpec with Matchers with ScalatestRouteTest {
+class STSApiTest extends WordSpec with DiagrammedAssertions with ScalatestRouteTest {
 
   class MockStsApi extends STSApi {
 
@@ -69,36 +69,36 @@ class STSApiTest extends WordSpec with Matchers with ScalatestRouteTest {
     "return an assume role - valid token in webIdentityTokenQuery parm" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$providerIdQuery$roleNameSessionQuery$arnQuery&WebIdentityToken=valid") ~>
         s3Routes ~> check {
-          status shouldEqual StatusCodes.OK
-          responseAs[String] shouldEqual "<assumeRoleWithWebIdentity></assumeRoleWithWebIdentity>"
+          assert(status == StatusCodes.OK)
+          assert(responseAs[String] == "<assumeRoleWithWebIdentity></assumeRoleWithWebIdentity>")
         }
     }
 
     "return an assume role - valid token in the header" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$providerIdQuery$roleNameSessionQuery$arnQuery$webIdentityTokenQuery") ~>
         validOAuth2TokenHeader ~> s3Routes ~> check {
-          status shouldEqual StatusCodes.OK
-          responseAs[String] shouldEqual "<assumeRoleWithWebIdentity></assumeRoleWithWebIdentity>"
+          assert(status == StatusCodes.OK)
+          assert(responseAs[String] == "<assumeRoleWithWebIdentity></assumeRoleWithWebIdentity>")
         }
     }
 
     "return an assume role - valid token in the cookie" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$providerIdQuery$roleNameSessionQuery$arnQuery$webIdentityTokenQuery") ~>
         validOAuth2TokenCookie ~> s3Routes ~> check {
-          status shouldEqual StatusCodes.OK
-          responseAs[String] shouldEqual "<assumeRoleWithWebIdentity></assumeRoleWithWebIdentity>"
+          assert(status == StatusCodes.OK)
+          assert(responseAs[String] == "<assumeRoleWithWebIdentity></assumeRoleWithWebIdentity>")
         }
     }
 
     "return rejection because missing the Action parameter" in {
       Get(s"/") ~> s3Routes ~> check {
-        rejections should contain atLeastOneElementOf List(MissingQueryParamRejection("Action"))
+        assert(rejections.contains(MissingQueryParamRejection("Action")))
       }
     }
 
     "return a bad request because the action in unknown" in {
       Get(s"/?Action=unknownAction") ~> s3Routes ~> check {
-        status shouldEqual StatusCodes.BadRequest
+        assert(status == StatusCodes.BadRequest)
       }
     }
 
@@ -109,66 +109,66 @@ class STSApiTest extends WordSpec with Matchers with ScalatestRouteTest {
           override protected[this] def canUserAssumeRole(userInfo: UserInfo, roleArn: String): Future[Boolean] =
             Future.successful(false)
         }.stsRoutes ~> check {
-          status shouldEqual StatusCodes.Forbidden
+          status == StatusCodes.Forbidden
         }
     }
 
     "return rejection because missing the RoleSessionName parameter" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$providerIdQuery$arnQuery$webIdentityTokenQuery") ~>
         s3Routes ~> check {
-          rejections should contain atLeastOneElementOf List(MissingQueryParamRejection("RoleSessionName"))
+          assert(rejections.contains(MissingQueryParamRejection("RoleSessionName")))
         }
     }
 
     "return rejection because missing the WebIdentityToken parameter" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$roleNameSessionQuery$arnQuery$roleNameSessionQuery") ~> s3Routes ~> check {
-        rejections should contain atLeastOneElementOf List(MissingQueryParamRejection("WebIdentityToken"))
+        assert(rejections.contains(MissingQueryParamRejection("WebIdentityToken")))
       }
     }
 
     "return rejection because missing the RoleArn parameter" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$roleNameSessionQuery$roleNameSessionQuery$webIdentityTokenQuery") ~> s3Routes ~> check {
-        rejections should contain atLeastOneElementOf List(MissingQueryParamRejection("RoleArn"))
+        assert(rejections.contains(MissingQueryParamRejection("RoleArn")))
       }
     }
 
     "return rejection because invalid token in param " in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$roleNameSessionQuery$arnQuery$roleNameSessionQuery$webIdentityTokenQuery") ~>
         s3Routes ~> check {
-          rejection shouldEqual AuthorizationFailedRejection
+          assert(rejection == AuthorizationFailedRejection)
         }
     }
 
     "return rejection because invalid token in the header" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$roleNameSessionQuery$arnQuery$roleNameSessionQuery$webIdentityTokenQuery") ~>
         invalidOAuth2TokenHeader ~> s3Routes ~> check {
-          rejection shouldEqual AuthorizationFailedRejection
+          assert(rejection == AuthorizationFailedRejection)
         }
     }
 
     "return rejection because invalid token in the cookie" in {
       Get(s"/$actionAssumeRoleWithWebIdentity$durationQuery$roleNameSessionQuery$arnQuery$roleNameSessionQuery$webIdentityTokenQuery") ~>
         invalidOAuth2TokenCookie ~> s3Routes ~> check {
-          rejection shouldEqual AuthorizationFailedRejection
+          assert(rejection == AuthorizationFailedRejection)
         }
     }
 
     "return a session token because valid credentials" in {
       Get(s"/$actionGetSessionToken") ~> validOAuth2TokenHeader ~> s3Routes ~> check {
-        status shouldEqual StatusCodes.OK
-        responseAs[String] shouldEqual "<getSessionToken></getSessionToken>"
+        assert(status == StatusCodes.OK)
+        assert(responseAs[String] == "<getSessionToken></getSessionToken>")
       }
     }
 
     "return rejection because invalid credentials" in {
       Get(s"/$actionGetSessionToken") ~> invalidOAuth2TokenHeader ~> s3Routes ~> check {
-        rejections should contain atLeastOneElementOf List(AuthorizationFailedRejection)
+        assert(rejections.contains(AuthorizationFailedRejection))
       }
     }
 
     "return rejection because no credentials" in {
       Get(s"/$actionGetSessionToken") ~> s3Routes ~> check {
-        rejections should contain atLeastOneElementOf List(AuthorizationFailedRejection)
+        assert(rejections.contains(AuthorizationFailedRejection))
       }
     }
 
@@ -186,26 +186,26 @@ class STSApiTest extends WordSpec with Matchers with ScalatestRouteTest {
       Post("/", FormData(queryToFormData(actionAssumeRoleWithWebIdentity, roleNameSessionQuery,
         arnQuery, providerIdQuery, roleNameSessionQuery) + ("WebIdentityToken" -> "valid"))) ~>
         s3Routes ~> check {
-          status shouldEqual StatusCodes.OK
+          assert(status == StatusCodes.OK)
         }
     }
 
     "return rejection because missing the Action parameter" in {
       Post("/") ~> s3Routes ~> check {
-        rejections should contain atLeastOneElementOf List(MissingQueryParamRejection("Action"))
+        assert(rejections.contains(MissingQueryParamRejection("Action")))
       }
     }
 
     "return a bad request because the action in unknown" in {
       Post("/", FormData("Action" -> "unknownAction")) ~> s3Routes ~> check {
-        status shouldEqual StatusCodes.BadRequest
+        assert(status == StatusCodes.BadRequest)
       }
     }
 
     "return rejection because missing the RoleSessionName parameter" in {
       Post("/", FormData(queryToFormData(actionAssumeRoleWithWebIdentity, providerIdQuery, durationQuery, arnQuery, webIdentityTokenQuery))) ~>
         s3Routes ~> check {
-          rejections should contain atLeastOneElementOf List(MissingFormFieldRejection("RoleSessionName"))
+          assert(rejections.contains(MissingFormFieldRejection("RoleSessionName")))
         }
     }
 
@@ -213,7 +213,7 @@ class STSApiTest extends WordSpec with Matchers with ScalatestRouteTest {
       Post("/", FormData(queryToFormData(actionAssumeRoleWithWebIdentity, roleNameSessionQuery,
         arnQuery, providerIdQuery, roleNameSessionQuery))) ~>
         s3Routes ~> check {
-          rejections should contain atLeastOneElementOf List(MissingFormFieldRejection("WebIdentityToken"))
+          assert(rejections.contains(MissingFormFieldRejection("WebIdentityToken")))
         }
     }
 
@@ -221,7 +221,7 @@ class STSApiTest extends WordSpec with Matchers with ScalatestRouteTest {
       Post("/", FormData(queryToFormData(actionAssumeRoleWithWebIdentity, roleNameSessionQuery,
         providerIdQuery, roleNameSessionQuery, webIdentityTokenQuery))) ~>
         s3Routes ~> check {
-          rejections should contain atLeastOneElementOf List(MissingFormFieldRejection("RoleArn"))
+          assert(rejections.contains(MissingFormFieldRejection("RoleArn")))
         }
     }
 
@@ -229,7 +229,7 @@ class STSApiTest extends WordSpec with Matchers with ScalatestRouteTest {
       Post("/", FormData(queryToFormData(actionAssumeRoleWithWebIdentity, roleNameSessionQuery,
         arnQuery, providerIdQuery, roleNameSessionQuery, webIdentityTokenQuery))) ~>
         s3Routes ~> check {
-          rejections should contain atLeastOneElementOf List(AuthorizationFailedRejection)
+          assert(rejections.contains(AuthorizationFailedRejection))
         }
     }
   }
