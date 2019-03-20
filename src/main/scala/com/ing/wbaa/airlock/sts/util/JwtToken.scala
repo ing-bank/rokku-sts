27e87a8c4abcd6f1d4ece88cab200ab471d69860
arempter <arempter@gmail.com>
2019-03-20 10:01:41 +0100

    add logging for token verification

--- src/main/scala/com/ing/wbaa/airlock/sts/util/JwtToken.scala
+++ src/main/scala/com/ing/wbaa/airlock/sts/util/JwtToken.scala
@@ -3,10 +3,11 @@ package com.ing.wbaa.airlock.sts.util
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.ing.wbaa.airlock.sts.config.StsSettings
+import com.typesafe.scalalogging.LazyLogging
 
-import scala.util.{Failure, Success, Try}
+import scala.util.{ Failure, Success, Try }
 
-trait JwtToken {
+trait JwtToken extends LazyLogging {
   protected[this] def stsSettings: StsSettings
 
   def verifyInternalToken(bearerToken: String): Boolean =
@@ -18,8 +19,14 @@ trait JwtToken {
       verifier.verify(bearerToken)
     } match {
       case Success(t) =>
-        if (t.getClaim("service").asString() == "airlock") true
-        else false
+        val serviceName = t.getClaim("service").asString()
+        if (serviceName == "airlock") {
+          logger.debug(s"Successfully verified internal token for $serviceName")
+          true
+        } else {
+          logger.debug(s"Failed to verify internal token")
+          false
+        }
       case Failure(exception) => throw exception
     }
 
