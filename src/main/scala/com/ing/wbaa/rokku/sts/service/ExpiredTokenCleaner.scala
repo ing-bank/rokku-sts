ee512f9ba791322f93f4b9e8e07def7c411531e1
Krzysztof Zmij <krzysztof.zmij@ingservicespolska.pl>
2019-04-26 10:08:22 +0200

    actor has been killed by Davide

--- src/main/scala/com/ing/wbaa/rokku/sts/service/ExpiredTokenCleaner.scala
+++ src/main/scala/com/ing/wbaa/rokku/sts/service/ExpiredTokenCleaner.scala
@@ -3,45 +3,27 @@ package com.ing.wbaa.rokku.sts.service
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 
-import akka.actor.{ Actor, ActorSystem, Props, Timers }
+import akka.actor.ActorSystem
 import com.ing.wbaa.rokku.sts.data.aws.AwsSessionTokenExpiration
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.concurrent.{ ExecutionContextExecutor, Future }
 import scala.concurrent.duration._
-import scala.concurrent.{ ExecutionContext, Future }
 
-trait ExpiredTokenCleaner {
+trait ExpiredTokenCleaner extends Runnable with LazyLogging {
 
   protected[this] implicit def system: ActorSystem
 
   protected[this] def cleanExpiredTokens(expirationDate: AwsSessionTokenExpiration): Future[Int]
 
-  system.actorOf(Props(new ExpiredTokenCleanActor(cleanExpiredTokens)), "expiredTokenCleanActor")
-}
+  protected[this] implicit val exContext: ExecutionContextExecutor = system.dispatcher
 
-object ExpiredTokenCleanActor {
+  system.scheduler.schedule(10.seconds, 1.day, this)
 
-  private case object CleanerKey
-
-  private case object CleanToken
-
-}
-
-class ExpiredTokenCleanActor(cleanExpiredTokens: AwsSessionTokenExpiration => Future[Int]) extends Actor with Timers with LazyLogging {
-
-  import ExpiredTokenCleanActor._
-
-  implicit val exContext: ExecutionContext = context.dispatcher
-
-  timers.startPeriodicTimer(CleanerKey, CleanToken, 1.day)
-
-  override def receive: Receive = {
-
-    case CleanToken =>
-      logger.debug("start clean expired tokens")
-      cleanExpiredTokens(AwsSessionTokenExpiration(Instant.now().minus(1, ChronoUnit.DAYS))).andThen {
-        case result => logger.debug("removed {} tokens", result)
-      }
-
+  override def run(): Unit = {
+    logger.debug("start clean expired tokens")
+    cleanExpiredTokens(AwsSessionTokenExpiration(Instant.now().minus(1, ChronoUnit.DAYS))).andThen {
+      case result => logger.debug("removed {} tokens", result)
+    }
   }
 }
