87ce33cef0d3e3c51d4108a3f2659639f5c7e666
arempter <arempter@gmail.com>
2019-08-27 14:04:36 +0200

    add requestId handling between rokku and sts

--- src/main/scala/com/ing/wbaa/rokku/sts/api/UserApi.scala
+++ src/main/scala/com/ing/wbaa/rokku/sts/api/UserApi.scala
@@ -4,18 +4,19 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.ing.wbaa.rokku.sts.data.aws.{ AwsAccessKey, AwsSessionToken }
-import com.ing.wbaa.rokku.sts.data.{ STSUserInfo, UserGroup }
+import com.ing.wbaa.rokku.sts.data.{ RequestId, STSUserInfo, UserGroup }
+import com.ing.wbaa.rokku.sts.handler.LoggerHandlerWithId
 import com.ing.wbaa.rokku.sts.util.JwtToken
-import com.typesafe.scalalogging.LazyLogging
 import spray.json.RootJsonFormat
 
 import scala.concurrent.Future
 
-trait UserApi extends LazyLogging with JwtToken {
+trait UserApi extends JwtToken {
 
   protected[this] def isCredentialActive(awsAccessKey: AwsAccessKey, awsSessionToken: Option[AwsSessionToken]): Future[Option[STSUserInfo]]
 
   val userRoutes: Route = isCredentialActive
+  private val logger = new LoggerHandlerWithId
 
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import spray.json.DefaultJsonProtocol._
@@ -29,28 +30,33 @@ trait UserApi extends LazyLogging with JwtToken {
     path("isCredentialActive") {
       get {
         headerValueByName("Authorization") { bearerToken =>
-
-          if (verifyInternalToken(bearerToken)) {
-
-            parameters(('accessKey, 'sessionToken.?)) { (accessKey, sessionToken) =>
-              onSuccess(isCredentialActive(AwsAccessKey(accessKey), sessionToken.map(AwsSessionToken))) {
-
-                case Some(userInfo) =>
-                  logger.info("isCredentialActive ok for accessKey={}, sessionToken={}", accessKey, sessionToken)
-                  complete((StatusCodes.OK, UserInfoToReturn(
-                    userInfo.userName.value,
-                    userInfo.userGroup.map(_.value),
-                    userInfo.awsAccessKey.value,
-                    userInfo.awsSecretKey.value)))
-
-                case None =>
-                  logger.warn("isCredentialActive forbidden for accessKey={}, sessionToken={}", accessKey, sessionToken)
-                  complete(StatusCodes.Forbidden)
-              }
+          optionalHeaderValueByName("x-rokku-request-id") { reqIdOpt =>
+            implicit val id = reqIdOpt match {
+              case Some(v) => RequestId(v)
+              case None    => RequestId("")
             }
-          } else {
-            logger.warn("isCredentialActive not verified for token={}", bearerToken)
-            complete(StatusCodes.Forbidden)
+
+            if (verifyInternalToken(bearerToken)) {
+              parameters(('accessKey, 'sessionToken.?)) { (accessKey, sessionToken) =>
+                onSuccess(isCredentialActive(AwsAccessKey(accessKey), sessionToken.map(AwsSessionToken))) {
+
+                  case Some(userInfo) =>
+                    logger.info("isCredentialActive ok for accessKey={}, sessionToken={}", accessKey, sessionToken)
+                    complete((StatusCodes.OK, UserInfoToReturn(
+                      userInfo.userName.value,
+                      userInfo.userGroup.map(_.value),
+                      userInfo.awsAccessKey.value,
+                      userInfo.awsSecretKey.value)))
+
+                  case None =>
+                    logger.warn("isCredentialActive forbidden for accessKey={}, sessionToken={}", accessKey, sessionToken)
+                    complete(StatusCodes.Forbidden)
+                }
+              }
+            } else {
+              logger.warn("isCredentialActive not verified for token={}", bearerToken)
+              complete(StatusCodes.Forbidden)
+            }
           }
         }
       }
