994456c8c26f98118d3073f6694cad16d87f1b43
Marcin Badura <marcin@badura.io>
2020-11-09 07:34:05 +0100

    Introduce k8s auth instead of vault token auth

--- src/main/scala/com/ing/wbaa/rokku/sts/config/VaultSettings.scala
+++ src/main/scala/com/ing/wbaa/rokku/sts/config/VaultSettings.scala
@@ -3,14 +3,18 @@ package com.ing.wbaa.rokku.sts.config
 import akka.actor.{ ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider }
 import com.typesafe.config.Config
 
+import scala.io.Source
+
 class VaultSettings(config: Config) extends Extension {
 
   val vaultUrl: String = config.getString("vault.url")
   val vaultPath: String = config.getString("vault.path")
-  val vaultToken: String = config.getString("vault.token")
   val readTimeout: Int = config.getInt("vault.read-timeout")
   val openTimeout: Int = config.getInt("vault.open-timeout")
   val retries: Int = config.getInt("vault.retries")
+  val jwt: String = Source.fromFile(config.getString("vault.service-account.token-location")).getLines().next()
+  val auth: String = config.getString("vault.service-account.auth-path")
+  val role: String = config.getString("vault.service-account.role")
 
 }
 
